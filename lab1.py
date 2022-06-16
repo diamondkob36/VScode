b7dcf36d44846081ffcd9e2004f16a0daaf7b9f8
diamond <diamondkob36@gmail.com>
2022-06-16 11:21:11 +0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,22 @@
+from logging import _Level
+
+
+print('Hello RumtSB Sci CS ITS 1654IN')
+#///////////////////
+n = 5
+if n == 10 :
+    print('n equal to 10')
+else:
+    print('n is something else excpet 10')\
+#//////////////////
+level = input('Enter level (1 - 4):')
+if level =='1':
+    print('easy')
+elif level =='2':
+    print('medium')
+elif level =='3':
+    print('hard')
+elif level =='4':
+    print('export')
+else:
+    print('Invalid level selected')
